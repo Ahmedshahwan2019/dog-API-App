0294f149e409191b8595c74afe75307a0fbb4bad
ahmed shahwan <shahwanahmed15@outlook.com>
2020-09-05 01:21:32 +0200

    added image to the dom from Api

--- app.js
+++ app.js
@@ -25,8 +25,35 @@ function createBreedList(breedList) {
 }
 
 async function loadByBreed(breed) {
-  if (breed !== 'Choose from dog breed') {
+  if (breed !== 'Choose dog breed') {
     const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
     const data = await response.json();
+    createSlideShow(data.message);
+  }
+}
+
+function createSlideShow(images) {
+  let currentPosition = 0;
+  const slideShow = document.getElementById('slide-show');
+  slideShow.innerHTML = `
+  <div  class="slide"  style="background-image: url('${images[0]}') "></div>
+  <div  class="slide"  style="background-image: url('${images[1]}') "></div>
+  `;
+  currentPosition += 2;
+  setInterval(nextSlide, 3000);
+  function nextSlide() {
+    slideShow.insertAdjacentElement(
+      'beforeend',
+      `  <div  class="slide"  style="background-image: url('${images[currentPosition]}') "></div>
+    `,
+    );
+    setTimeout(function () {
+      document.querySelector('.slide').remove();
+    }, 1000);
+    if (currentPosition + 1 >= images.length) {
+      currentPosition = 0;
+    } else {
+      currentPosition++;
+    }
   }
 }
